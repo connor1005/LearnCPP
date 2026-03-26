f1ab37669ef517e6f81460d56232136385b770e0
Connor Ingram <redeagle225@gmail.com>
2026-03-26 12:11:14 -0400

    pointers

--- pointers.cpp
+++ pointers.cpp
@@ -2,9 +2,31 @@
 
 int main()
 {
-    int x = 5;
-    int* pX = &x;
+    int x{5};
+    int* ptr;       //uninitialized pointer (holds a garbage address)
+    int* ptr2{};    //null pointer
+    int* ptr3{&x};  //pointer initialized with the address of var x
 
-    std::cout << "Variable: " << x << "\nMemory Address: " << pX << std::endl;
+    std::cout << x << std::endl;
+    std::cout << ptr3 << "\n";
+
+    int y{5};
+    int& ref{y};
+    int* pointer{&y};
+
+    std::cout << y;
+    std::cout << ref;
+    std::cout << pointer << " is the memory address that holds " << *pointer << '\n';
+    
+    ref = 6;     //uses the reference to change the value of y
+    std::cout << y;
+    std::cout << ref;
+    std::cout << pointer << " holds " << *pointer;
+
+    *pointer = 7;   //uses the pointer to change the value of y
+    std::cout << y;
+    std::cout << ref;
+    std::cout << *pointer << '\n';
+
     return 0;
 }
